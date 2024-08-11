ae6ff69f39d21423bc14e10494f636f45eac1f0e
Biswajit Debnath <biswajit.basanti@gmail.com>
2024-08-11 16:08:26 +0530

    review in home page added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,45 @@
-"use client";
+// "use client";
 import React from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Input,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  Link,
-  Button,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-} from "@nextui-org/react";
-import { AcmeLogo } from "./AcmeLogo.jsx";
-import {
-  ChevronDown,
-  Lock,
-  Activity,
-  Flash,
-  Server,
-  TagUser,
-  Scale,
-} from "./Icons.jsx";
-import { Accordion, AccordionItem } from "@nextui-org/accordion";
-import { FaArrowRight } from "react-icons/fa";
+import config from "../../config.js";
+import NextUiNavbar from "./NextUInav";
 
-export default function App() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
+
+const fetchNames = async () => {
+
+  const reqOptions = {
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`
+    },
+    cache: 'no-store'as RequestCache,
+  }
+  const request = await fetch(`${config.api}/api/reviews?fields[0]=vpn_name&fields[1]=slug`, reqOptions);
+  const response = await request.json();
+
+  return response.data;
+}
+
+
+// Build subnav from API data
+const buildSubnavFromApi = async () => {
+  
+  const apiData = await fetchNames();
+
+  // Convert API data into subnav items
+  const subnavFromApi = apiData.map((item: { attributes: { vpn_name: string; slug: string } }) => ({
+    name: item.attributes.vpn_name,
+    link: `/reviews/${item.attributes.slug}`, // Customize the link format as needed
+  }))
+  .slice(0, 4); 
+  
+  return subnavFromApi;
+};
+
+
+const NavbarComp = async () => {
+  // const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const rev = await buildSubnavFromApi();
+
 
   interface SubnavItem {
     // Define properties based on your actual structure
@@ -53,276 +49,92 @@ export default function App() {
 
   interface NavItem {
     mainHeading: string;
-    link: string;
     subnav: SubnavItem[];
   }
 
   const navdata: NavItem[] = [
     {
-      mainHeading: "BEST VPN",
-      link: "",
-      subnav: [
-        {
-          name: "Best VPN of 2024",
-          link: "Link1",
-        },
-        {
-          name: "Best Free VPN",
-          link: "Link2",
-        },
-        {
-          name: "Best VPN Free Trials",
-          link: "Link3",
-        },
-        {
-          name: "Best Cheap VPN",
-          link: "Link4",
-        },
-        {
-          name: "Fastest VPN's of 2024",
-          link: "Link5",
-        },
-      ],
-    },
-    {
       mainHeading: "VPN REVIEWS",
-      link: "",
       subnav: [
-        {
-          name: "ExpressVPN",
-          link: "Link1",
-        },
-        {
-          name: "NordVPN",
-          link: "Link2",
-        },
-        {
-          name: "Surfshark",
-          link: "Link3",
-        },
-        {
-          name: "CyberGhost",
-          link: "Link4",
-        },
-        {
-          name: "PrivateVPN",
-          link: "Link5",
-        },
+        ...rev,
       ],
     },
     {
       mainHeading: "WHAT IS A VPN",
-      link: "",
       subnav: [
         {
           name: "What Is a VPN Service?",
-          link: "Link1",
+          link: "/vpn-explained/what-is-a-vpn-service",
         },
         {
           name: "How Does a VPN Work?",
-          link: "Link2",
+          link: "/vpn-explained/how-does-a-vpn-work",
         },
         {
           name: "What Is a VPN Used For?",
-          link: "Link3",
+          link: "/vpn-explained/what-is-vpn-used-for",
         },
         {
           name: "Are VPNs Really Worth It?",
-          link: "Link4",
+          link: "/vpn-explained/are-vpn-really-worth-it",
         },
         {
           name: "VPN Logging Policies",
-          link: "Link5",
+          link: "/vpn-explained",
         },
       ],
     },
     {
       mainHeading: "GUIDES",
-      link: "",
       subnav: [
         {
           name: "How To Hide Your IP Address",
-          link: "Link1",
+          link: "/guides/hide-your-ip-address",
         },
         {
           name: "How To Check Your IP Address",
-          link: "Link2",
+          link: "/guides/check-ip-address",
         },
+        {
+          name: "What can someone do with your ip address",
+          link: "/guides/what-can-people-do-with-your-ip-address",
+        },
+        {
+          name: "How to check if your vpn is working?",
+          link: "/guides/is-my-vpn-working",
+        },
+        {
+          name: "How To Check Your IP Address",
+          link: "/guides",
+        }
       ],
     },
     {
       mainHeading: "ABOUT",
-      link: "",
       subnav: [
         {
           name: "About Us",
-          link: "/aboutus",
+          link: "/about",
         },
         {
           name: "How we test and review",
-          link: "Link2",
+          link: "/about/how-we-review-vpn",
         },
         {
           name: "How we make money",
-          link: "/howWeMakeMoney",
+          link: "/about/how-we-make-money",
         },
       ],
     },
   ];
 
-  const icons = {
-    chevron: (
-      <ChevronDown
-        fill="currentColor"
-        size={16}
-        height={undefined}
-        width={undefined}
-      />
-    ),
-    scale: (
-      <Scale
-        className="text-warning"
-        fill="currentColor"
-        size={30}
-        height={undefined}
-        width={undefined}
-      />
-    ),
-    lock: (
-      <Lock
-        className="text-success"
-        fill="currentColor"
-        size={30}
-        height={undefined}
-        width={undefined}
-      />
-    ),
-    activity: (
-      <Activity
-        className="text-secondary"
-        fill="currentColor"
-        size={30}
-        height={undefined}
-        width={undefined}
-      />
-    ),
-    flash: (
-      <Flash
-        className="text-primary"
-        fill="currentColor"
-        size={30}
-        height={undefined}
-        width={undefined}
-      />
-    ),
-    server: (
-      <Server
-        className="text-success"
-        fill="currentColor"
-        size={30}
-        height={undefined}
-        width={undefined}
-      />
-    ),
-    user: (
-      <TagUser
-        className="text-danger"
-        fill="currentColor"
-        size={30}
-        height={undefined}
-        width={undefined}
-      />
-    ),
-  };
+
+
+
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen} className="W-full">
-      <NavbarContent justify="center">
-        {/* <NavbarContent className="hidden sm:flex gap-4" justify="center"> */}
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="laptop:hidden"
-        />
-        <Link href="/" className="text-inherit"><NavbarBrand className="max-w-fit">
-          <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
-        </NavbarBrand></Link>
-
-        <NavbarContent className="hidden laptop:flex gap-3">
-          {navdata.map((item, idx) => (
-            <Dropdown>
-              <NavbarItem>
-                <DropdownTrigger>
-                  <Button
-                    disableRipple
-                    className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                    endContent={icons.chevron}
-                    radius="sm"
-                    variant="light"
-                  >
-                    {item.mainHeading}
-                  </Button>
-                </DropdownTrigger>
-              </NavbarItem>
-              <DropdownMenu
-                aria-label="ACME features"
-                className="w-[340px]"
-                itemClasses={{
-                  base: "gap-5",
-                }}
-              >
-                {item.subnav.slice(0, 5).map((subitem, subidx) => (
-                  <DropdownItem
-                    // key="autoscaling"
-                    // description="ACME scales apps to meet user demand, automagically, based on load."
-                    className={`${subidx == 4 ? "border-1 border-grey" : ""}`}
-                    startContent={subidx == 5 ? null : icons.flash}
-                  >
-                    {subidx == 4 ? (
-                      <button className="font-semibold flex justify-center text-[#197BEB]  items-center gap-3">
-                        click more
-                        <FaArrowRight className="" />
-                      </button>
-                    ) : (
-                      <Link href={subitem.link}>{subitem.name}</Link>
-                    )}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-          ))}
-          <NavbarItem>
-            <Link href="/contactpage">
-              <Button
-                disableRipple
-                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                radius="sm"
-                variant="light"
-              >
-                CONTACT
-              </Button>
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-      </NavbarContent>
-
-      <NavbarMenu>
-        <Accordion>
-          {navdata.map((itm, idx) => (
-            <AccordionItem
-              key={idx}
-              aria-label={itm.mainHeading}
-              title={itm.mainHeading}
-            >
-              {itm.subnav.slice(0, 6).map((subitm, subidx) => (
-                <div>
-                  {subidx == 5 ? <button>see more</button> : subitm.name}
-                </div>
-              ))}
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </NavbarMenu>
-    </Navbar>
+    <NextUiNavbar navdata={navdata} />
   );
 }
+
+export default NavbarComp;
--- src/pages/ReviewsPage.tsx
+++ src/pages/ReviewsPage.tsx
@@ -7,12 +7,12 @@ import { IoLogoAndroid } from "react-icons/io";
 import { FaLinux, FaWindows } from "react-icons/fa";
 import { MdRouter } from "react-icons/md";
 import { SiMacos } from "react-icons/si";
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import config from '../../config.js'
-import { cache } from "react";
 import CustomBreadcrumb from "@/components/Breadcrumb";
+import ReviewCard from "@/components/ReviewCard.jsx";
 
-const fetchBlogs = async () => {
+
+export const fetchBlogs = async () => {
 
   const reqOptions = {
     headers: {
@@ -23,12 +23,13 @@ const fetchBlogs = async () => {
   const request = await fetch(`${config.api}/api/reviews?fields[0]=vpn_name&fields[1]=ratting&fields[2]=slug&fields[3]=offer&fields[4]=details&populate[features]=*&populate[logo]=*&populate[compnay_link]=*&populate[top_banner]=*`, reqOptions);
   const response = await request.json();
 
-
+  
   return response.data;
 }
 
+
 const ReviewsPage = async () => {
-  const blogs = await fetchBlogs();
+   const blogs = await fetchBlogs();
 
   return (
     <div className="bg-gray-100">
@@ -60,101 +61,7 @@ const ReviewsPage = async () => {
       {/* review section */}
       {
         blogs ?
-          <section className="px-3 tablet:px-5 flex flex-col gap-7">
-
-            {/* review card */}
-            {
-              blogs.map((blog, idx) => (
-                <div className="shadow-[0px_0px_10px_0px_#d1d5db] rounded-md bg-white">
-
-                  {/* top banner */}
-                  <div className="bg-red-500 w-fit rounded-tl-md flex items-center text-xs laptop:text-sm rounded-br-md">
-                    <div className="flex justify-center items-center px-3 py-1">{idx + 1}</div>
-                    {
-                      blog.attributes.top_banner.map((subitm, subidx) => (
-                        <>
-                          <div className={`bg-yellow-500  w-fit px-3 py-1 h-full ${subidx == 0 ? 'rounded-tl-md' : ''} ${blog.attributes.top_banner.length == subidx + 1 ? 'rounded-br-md' : ''}`}>{subitm.value}</div>
-                          {
-                            blog.attributes.top_banner.length == subidx + 1 ?
-                              null
-                              :
-                              <span className="bg-yellow-500 py-1">|</span>
-                          }
-                        </>
-                      ))
-                    }
-
-                  </div>
-                  <div className="flex laptop:flex-row flex-col">
-
-                    {/* vpn image */}
-                    <div className="flex flex-col laptop:flex-col tablet:flex-row tablet:border-b laptop:border-none border-gray-400 laptop:w-[30%]">
-
-                      <div className=" flex justify-center tablet:justify-center  mb-2 border-b tablet:border-none border-gray-400 tablet:w-1/2 laptop:w-full ">
-                        <Link href={`/reviews/${blog.attributes.slug}`} className="relative aspect-square w-3/4 tablet:w-1/2">
-                          <Image src={`${config.api}${blog.attributes.logo.data.attributes.url}`} fill alt='express' />
-                        </Link >
-                      </div>
-
-                      {/* vpn details */}
-                      <div className="flex flex-col justify-center px-3 mb-3 tablet:w-1/2 laptop:w-full">
-                        {/* <p className="text-sm font-medium pb-1">{blog.attributes.vpn_name}</p> */}
-                        <p className="text-sm font-medium pb-1">{blog.attributes.details}</p>
-                        <p className="tablet:mt-2 text-sm font-semibold text-blue-600">{blog.attributes.offer}</p>
-                      </div>
-                    </div>
-
-                    <div className="laptop:w-[40%]">
-                      <ul className="ml-6 mr-3 space-y-2 tablet:my-4">
-                        {
-                          blog.attributes.features.map((subitm, subidx) => (
-                            <li className="tick-list-item">{subitm.value}</li>
-                          ))
-                        }
-                      </ul>
-
-                      <div className="hidden tablet:flex gap-5 text-gray-400 pr-3 pl-6 text-2xl my-3">
-                        <FaWindows />
-                        <SiMacos />
-                        <IoLogoAndroid />
-                        <FaLinux />
-                        <MdRouter />
-                      </div>
-                    </div>
-
-                    {/* bottom */}
-                    <div className="flex  my-3 laptop:w-[30%] laptop:items-center">
-
-                      {/* ratting */}
-                      <div className="flex flex-col items-center ml-3 gap-2 w-1/2">
-                        <CustomCircularProgress
-                          size="lg"
-                          value={blog.attributes.ratting}
-                          color={`${blog.attributes.ratting >= 9.5 ? 'warning' : 'primary'}`}
-                          showValueLabel={true}
-                          valueLabel={`${blog.attributes.ratting}`} // Pass the value without the percentage sign
-                          className="customRating"
-                        />
-                        <p className={`text-xs laptop:text-sm  font-bold ${blog.attributes.ratting >= 9.5 ? 'text-[#c4841d]' : 'text-[#004493]'}`}>
-                          {
-                            blog.attributes.ratting >= 9.5 ? "OUTSTANDING!" : "SUPERB!"
-                          }
-
-                        </p>
-                      </div>
-
-                      <div className="w-1/2 mr-3 flex justify-center items-center flex-col gap-2">
-                        <Link href={`/reviews/${blog.attributes.slug}`} className="bg-red-700 text-white font-bold px-3 py-1 rounded-lg">Visit website</Link>
-                        <Link href={`${blog.attributes.compnay_link.value}`} className="underline text-gray-500">{blog.attributes.compnay_link.name}</Link>
-                      </div>
-                    </div>
-                  </div>
-
-                </div>
-              ))
-            }
-
-          </section>
+          <ReviewCard blogs={blogs}/>
           :
           "waiting"
 
