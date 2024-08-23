cc54366582337f08db0c1bfbfc4ceed4cab0c7c5
VPN Guide <mhvpnguide@gmail.com>
2024-08-23 13:02:29 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -53,8 +53,7 @@ const Footer = () => {
           <Link href="/terms-conditions" className="text-sm hover:text-blue-700 pr-2">Terms of Use</Link>
         </div>
         <p className="text-sm mb-4">
-          vpnguide.in is operated by <Link href="https://velocityclick.in/" className="italic hover:text-blue-700">Velocity Click </Link>.
-        <p className="text-sm mb-4"> 
+          vpnguide.in is operated by <Link href="https://velocityclick.in/" className="italic hover:text-blue-700">Velocity Click </Link>. | |
           vpnguide.in is made by <Link href="https://nbtservices.framer.website/" className="italic hover:text-blue-700">Next Big Thing </Link>.| 
           © 2016-2024. All rights reserved.</p>
       </div>
