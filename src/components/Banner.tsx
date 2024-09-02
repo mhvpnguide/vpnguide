24a9ff402c5bba1615ff6a398c90607d9a75686d
VPN Guide <mhvpnguide@gmail.com>
2024-09-02 17:57:11 +0530

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -14,7 +14,7 @@ const Banner = () => {
                 Take control of your internet privacy and security with our <Link href="/reviews" className="text-bluelink hover:underline">unbiased VPN reviews</Link> and independent advice.
             </div>
             <div className="w-[280px] h-[280px] relative">
-                <Image src="/Assests/Homepage/Homeimg.png" alt="VPN Banner" fill className="mt-4 tablet:mt-6 laptop:mt-8" />
+                <Image src="/Assests/Homepage/Homepage_Header.png" alt="VPN Banner" fill className="mt-4 tablet:mt-6 laptop:mt-8" />
             </div>
             <div className="flex flex-col w-full px-3 tablet:grid tablet:grid-cols-1 tablet:w-1/2 laptop:grid laptop:grid-cols-2 laptop:w-2/3 laptopl:flex laptopl:flex-row laptopl:w-full 4k:flex 4k:flex-row 4k:w-1/2 gap-4 mt-4 tablet:mt-6 laptop:mt-8 ">
                 <div className="bg-white w-full pl-[12px] p-3 pr-[20px] mb-3 font-semibold rounded-3xl shadow-custom transition transform active:-translate-y-2 active:shadow-custom-active text-black flex justify-start items-center gap-3 text-center cursor-pointer hovereff">
