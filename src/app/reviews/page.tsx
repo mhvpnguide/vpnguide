ff83aa7625215dad5763196239c9ab407a926399
VPN Guide <mhvpnguide@gmail.com>
2024-09-10 13:51:16 +0530

    Update page.tsx

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -2,7 +2,8 @@ import ReviewsPage from "../../pages/ReviewsPage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Reviews',
+  title: 'VPNs Reviews - VPNs Guide',
+  description: 'Read detailed Reviews about our top best VPNs',
 };
 const reviews = () => {
     return (
