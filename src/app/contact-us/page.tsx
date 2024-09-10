90d03291e5618d50bf57d0d855f6b8aa2a8719a8
VPN Guide <mhvpnguide@gmail.com>
2024-09-10 13:57:53 +0530

    Update page.tsx

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -2,7 +2,8 @@ import ContactPage from "../../pages/ContactPage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Contact',
+  title: 'Contact Us - VPNs Guide',
+  description: 'Get in touch with us for any inquiries, support, or feedback. We are here to help you with any questions you may have.Here are other ways to connect with us: Call: +91 6291925441; Mail: mhvpnguide@gmail.com; Location: Kolkata, West Bengal, India.',
 };
 const contactPage = ()=>{
     return(
