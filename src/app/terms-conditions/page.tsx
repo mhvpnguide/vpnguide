add6f94c9e4f92c73e01ccd5a93ca03e74e5beb9
VPN Guide <mhvpnguide@gmail.com>
2024-09-10 13:42:47 +0530

    Update page.tsx

--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -2,7 +2,8 @@ import Term_Condition from "../../pages/Term_Condition";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'terms',
+  title: 'Terms and Conditions - VPNs Guide',
+  description: 'Read the terms and conditions of using our services. Learn about your rights and responsibilities when using our website.',
 };
 const termCondition = ()=>{
     return(
