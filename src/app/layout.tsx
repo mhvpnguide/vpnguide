b1a3fb5afbc6a0af14243038c0f7cebd7c9c7f33
VPN Guide <mhvpnguide@gmail.com>
2024-09-20 11:22:47 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -274,7 +274,10 @@ export default function RootLayout({
         </script>
 
           <!-- Google Tag Manager -->
-         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+         <script>
+           dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -283,6 +286,7 @@ export default function RootLayout({
          <!-- End Google Tag Manager -->
         
       </head>
+            
       <body className={inter.className}>
         <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TV8QXFD7"
@@ -296,6 +300,7 @@ export default function RootLayout({
         </NextUIProvider>
       </body>
       <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+      <GoogleTagManager gtmId= 'GTM-TV8QXFD7'  />
     </html>
   );
 }
