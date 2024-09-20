5cd0d03dd4b8bbd611f2cad347933ed030f5005c
VPN Guide <mhvpnguide@gmail.com>
2024-09-20 10:37:18 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -261,6 +261,18 @@ export default function RootLayout({
             `,
           }}
         />
+
+        <!-- Google tag (gtag.js) -->
+         <script async src="https://www.googletagmanager.com/gtag/js?id=G-V3J9MTSWW4">
+         </script>
+         <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-V3J9MTSWW4');
+        </script>
+        
       </head>
       <body className={inter.className}>
         <NextUIProvider>
