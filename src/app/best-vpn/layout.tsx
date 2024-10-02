d6867c64d5888b8f76a0de77fbb12f8d2a4f37aa
VPN Guide <mhvpnguide@gmail.com>
2024-10-02 12:18:48 +0530

    tracking code added.tsx

--- src/app/best-vpn/layout.tsx
+++ src/app/best-vpn/layout.tsx
@@ -6,8 +6,27 @@ export default function bestVpnLayout({
     return (
         <section>
             <head>
+                <title>Best VPN of 2024</title>
             </head>
+            
+            <body>
+                {/* <!-- Tracking (script) --> */}
+                //Configuration script
+                  <script>
+                    var clpconfig = {"clroot" : "https://track.vpns.guide/",  "clcsr" : "1",};
+                  </script>
+                
+                 //external script
+                  <script src="https://track.vpns.guide/landing.js">
+                  </script>
+
+                 //function call
+                 <script>checkdirect(8,1)
+                 </script>
+                {/* <!-- Tracking (script) END--> */}
+                
             {children}
+            </body>
         </section>
     )
 }
