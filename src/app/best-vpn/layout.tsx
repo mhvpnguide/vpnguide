d5c96856d926f17e6626454c19ae7dfa705a30a1
VPN Guide <mhvpnguide@gmail.com>
2024-10-02 13:41:51 +0530

    Update layout.tsx

--- src/app/best-vpn/layout.tsx
+++ src/app/best-vpn/layout.tsx
@@ -14,13 +14,35 @@ export default function bestVpnLayout({
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-                            var clpconfig = {"clroot" : "https://track.vpns.guide/", "clcsr" : "1"};
-                            checkdirect(8,3);
+                            var clpconfig = {
+                            "clroot" : "https://track.vpns.guide/",
+                            "engsec" : "30",
+                            "clcsr" : "1"
+                            };
                         `,
                     }}
                 />
+                
                 <script src="https://track.vpns.guide/landing.js" async />
+
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        checkdirect(8,3);
+                         `,
+                    }}
+                />
+                        
+                
                 {/* <!-- Tracking (script) END--> */}
+
+                {/* <!-- Tracking (script) --> */}
+        <script>
+          {`
+            checkdirect(8,1)
+          `}
+        </script>
+        {/* <!-- Tracking (script) END--> */}
                 
             {children}
             </body>
