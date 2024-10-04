cf55aa7418a98e621068c301dea4705ff9f485fa
VPN Guide <mhvpnguide@gmail.com>
2024-10-04 16:57:33 +0530

    Update BestVpnIndia layout.tsx

--- src/app/best-vpn-for-india/layout.tsx
+++ src/app/best-vpn-for-india/layout.tsx
@@ -9,57 +9,7 @@ export default function bestVpnForIndiaLayout({
 }) {
     return (
         <section>
-            <head>
-            </head>
-            <body>
-                
                {children}
-
-            {/* <!-- Cpa Tracking (script) --> */}
-                <Script
-                    id="clpconfig"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            var clpconfig = {
-                            "clroot" : "https://track.vpns.guide/",
-                            "clcsr" : "1"
-                            };
-                        `,
-                    }}
-                   />
-                
-                <Script 
-                    src="//track.vpns.guide/landing.js" 
-                    strategy="afterInteractive"
-                />
-
-                <Script
-                    id="checkdirect"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        checkdirect(21,1);
-                         `,
-                    }}
-                />
-                        
-                
-                {/* <!-- Cpa Tracking (script) END--> */}
-
-                {/* <!-- Cpa Tracking (script) --> */}
-                   <Script
-                    id="checkdirect-inline"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            checkdirect(21,1);
-                        `,
-                    }}
-                  />
-               {/* <!-- Cpa Tracking (script) END--> */}
-
-            </body>
         </section>
     )
 }
