9a2de5ea4d0f811b921d81bc60a6e6526a38bea4
Biswajit Debnath <biswajit.basanti@gmail.com>
2024-10-04 14:49:39 +0530

    new comment add

--- src/app/best-vpn/layout.tsx
+++ src/app/best-vpn/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Script from "next/script";
 
+
 export default function bestVpnLayout({
     children, // will be a page or nested layout
 }: {
@@ -12,19 +13,25 @@ export default function bestVpnLayout({
                 <title>Best VPN of 2024</title>
 
              {/* <!-- Tracking (script) --> */}
-                <script>
-                  window.clickmagick_cmc = 
-                  {
-                    uid: '202178',
-                    hid: '3519712257',
-                    cmc_project: 'test',
-                    vid_info: 'on',
-                    utm_source: 'organic',
-                  }
-             </script>
-
-               <script src='//cdn.clkmc.com/cmc.js'>
-               </script>
+             <Script
+                    id="clickmagick-cmc"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                          window.clickmagick_cmc = {
+                            uid: '202178',
+                            hid: '3519712257',
+                            cmc_project: 'test',
+                            vid_info: 'on',
+                            utm_source: 'organic',
+                          };
+                        `,
+                    }}
+                />
+                <Script
+                    src="//cdn.clkmc.com/cmc.js"
+                    strategy="afterInteractive"
+                />
              {/* <!-- Tracking (script) END--> */} 
 
             </head>
