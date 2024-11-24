492d7d28398d9856fdd204a41a02c4ea74a7f34a
harsh149xxx <71449572+harsh149xxx@users.noreply.github.com>
2024-11-25 00:05:27 +0530

    Update VpnPages.tsx featured remove complete

--- src/pages/VpnPages.tsx
+++ src/pages/VpnPages.tsx
@@ -73,15 +73,7 @@ const vpnData: VPNData =
     ],
   
 
-    {/* // These VPN Services have been featured on:
-    featuredImage:
-        [
-            { link: "NewYorkTimes.png" },
-            { link: "FoxNews.png" },
-            { link: "Bbc.png" },
-            { link: "Cnn.png" },
-            { link: "CNet.png" }
-        ], */}
+
     data: [
         {
             "attributes": {
@@ -351,32 +343,7 @@ const TestVpnPage = () => {
                         } */}
                     </ul>
 
-                    {/* featured section
-                    <div className="hidden tablet:block">
-                        <span className="font-kantumruyPro">These VPN Services have been featured on:</span>
-                        <div className="mb-1 ml-3 mt-[10px] flex items-center gap-5">
-                            <div className="relative h-[30px] w-[150px]">
-                                <Image src={`/Assests/test-vpn/NYT.svg`} alt="New York Times"
-                                    className="w-full" fill />
-                            </div>
-                            <div className="relative h-[30px] w-[30px]">
-                                <Image src={`/Assests/test-vpn/cnet.svg`} alt="CNET"
-                                    className="w-full" fill />
-                            </div>
-                            <div className="relative h-[30px] w-[58px]">
-                                <Image src={`/Assests/test-vpn/BBC.svg`} alt="BBC"
-                                    className="w-full" fill />
-                            </div>
-                            <div className="relative h-[30px] w-[40px]">
-                                <Image src={`/Assests/test-vpn/CNN.svg`} alt="CNN"
-                                    className="w-full" fill />
-                            </div>
-                            <div className="relative h-[30px] w-[30px]">
-                                <Image src={`/Assests/test-vpn/FoxNews.svg`} alt="Fox News"
-                                    className="w-full" fill />
-                            </div> 
-                        </div> 
-                    </div> */}
+                    
                 </div>
 
                 {/* right */}
