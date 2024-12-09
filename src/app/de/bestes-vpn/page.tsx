c92474cfa81f8a78b34350a255aa2b42e7012581
harsh149 <harshaga149@gmail.com>
2024-12-09 07:58:52 +0530

    tiles update

--- src/app/de/bestes-vpn/page.tsx
+++ src/app/de/bestes-vpn/page.tsx
@@ -1,18 +1,19 @@
 import BestesVpnPage from "@/pages/BestesVpnPage";
 import { Metadata } from "next";
+import Head from "next/head";
 
-export const metadata: Metadata = {
-  title: 'Bestes VPN of Vpns Guide',
-  description: 'Learn more about our company, our mission, and the team behind our success.',
-  keywords: 'VPN, Best Vpn, VPNs Guide, Private browsing, Online security, VPN services, buy, sell, discount',
-  authors: [{ name: 'VPNsGuide' }],
-  viewport: 'width=device-width, initial-scale=1',
-  robots: 'index, follow',
+const BestVpn = () => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Learn more about our company, our mission, and the team behind our success." />
+        <meta name="keywords" content="VPN, Best Vpn, VPNs Guide, Private browsing, Online security, VPN services, buy, sell, discount" />
+        <meta name="robots" content="index, follow" />
+      </Head>
+      <BestesVpnPage />
+    </>
+  );
 };
-const bestVpn = ()=>{
-    return(
-        <BestesVpnPage/>
-    )
-}
 
-export default bestVpn;
+export default BestVpn;
