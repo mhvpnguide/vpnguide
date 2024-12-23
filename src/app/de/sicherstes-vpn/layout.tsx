4464472b21554513e5c125ce96e82ad34bcde78e
harsh149 <harshaga149@gmail.com>
2024-12-23 22:43:18 +0530

    optinmonster

--- src/app/de/sicherstes-vpn/layout.tsx
+++ src/app/de/sicherstes-vpn/layout.tsx
@@ -1,47 +1,61 @@
-import DeSpecialNavbar from "@/components/DeSpecialNavbar"
-import Script from "next/script"
+import DeSpecialNavbar from "@/components/DeSpecialNavbar";
+import Script from "next/script";
 
 export default function testVpnLayoutreviewLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <>
-    {/* canonical tag */}
-    <link rel="canonical" href="https://vpns.guide/de/bestes-vpn" />
+      {/* Canonical tag */}
+      <link rel="canonical" href="https://vpns.guide/de/bestes-vpn" />
       <section>
-        <DeSpecialNavbar/>
+        <DeSpecialNavbar />
         {children}
       </section>
-      {/* <!-- Cpa Tracking (script) --> */}
-<Script
-                id="clpconfig"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
+      {/* CPA Tracking Scripts */}
+      <Script
+        id="clpconfig"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
                             var clpconfig = {
                             "clroot" : "https://track.vpns.guide/",
                             "engsec" : "60",
                             "clcsr" : "1"
                             };
                         `,
-                }}
-            />
-            <Script
-                src="//track.vpns.guide/landing.js"
-                strategy="afterInteractive"
-            />
-            <Script
-                id="checkdirect"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
+        }}
+      />
+      <Script src="//track.vpns.guide/landing.js" strategy="afterInteractive" />
+      <Script
+        id="checkdirect"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
                         setTimeout("checkdirect(42,2)",2000);
                          `,
-                }}
-            />
-            {/* <!-- Cpa Tracking (script) END--> */}
+        }}
+      />
+      {/* OptinMonster Script */}
+      <Script
+        id="optinmonster-script"
+        strategy="lazyOnload" // Lazy load the script after page content has loaded
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(d,u,ac){
+            var s=d.createElement('script');
+            s.type='text/javascript';
+            s.src='https://a.omappapi.com/app/js/api.min.js';
+            s.async=true;
+            s.dataset.user=u;
+            s.dataset.account=ac;
+            d.getElementsByTagName('head')[0].appendChild(s);
+          })(document, 352620, 372811);
+        `,
+        }}
+      />
     </>
-  )
+  );
 }
