60beb8e2944e9f4d3c76a68b8836a3a5ebe8ed06
harsh149 <harshaga149@gmail.com>
2024-12-27 23:03:31 +0530

    india

--- src/app/in/best-cheap-vpn/layout.tsx
+++ src/app/in/best-cheap-vpn/layout.tsx
@@ -38,6 +38,7 @@ export default function testVpnLayoutreviewLayout({
                 dangerouslySetInnerHTML={{
                     __html: `
                         setTimeout("checkdirect(44,2)",2000);
+                        setTimeout("checkdirect(46,2)",2000);
                          `,
                 }}
             />
