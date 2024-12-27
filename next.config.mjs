d5b2f631e7dcf6140df3d3d8a4b6edfe6a4e598c
harsh149 <harshaga149@gmail.com>
2024-12-28 02:21:22 +0530

    india

--- next.config.mjs
+++ next.config.mjs
@@ -12,14 +12,27 @@ const nextConfig = {
             }
         ]
     },
-    redirects:async()=>{
-        return[
+    redirects: async () => {
+        return [
             {
-                source:"/admin",
-                destination:"https://shark-app-4dsd9.ondigitalocean.app/admin",
-                permanent:true
+                source: "/admin",
+                destination: "https://shark-app-4dsd9.ondigitalocean.app/admin",
+                permanent: true
             }
-        ]
+        ];
+    },
+    webpack(config) {
+        // Prevent minification or optimization of the OptinMonster script
+        config.optimization = {
+            ...config.optimization,
+            minimize: true,
+            minimizer: [
+                new (require('terser-webpack-plugin'))({
+                    exclude: /a\.omappapi\.com/, // Exclude the OptinMonster script
+                }),
+            ],
+        };
+        return config;
     }
 };
 
