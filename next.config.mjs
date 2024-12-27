278958453290b0c46fe6912d76d5698b817d081e
harsh149 <harshaga149@gmail.com>
2024-12-28 03:02:40 +0530

    optin back

--- next.config.mjs
+++ next.config.mjs
@@ -1,45 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "*",
-        },
-        {
-          protocol: "http",
-          hostname: "*",
-        }
-      ]
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*",
+            },
+            {
+                protocol: "http",
+                hostname: "*",
+            }
+        ]
     },
-    redirects: async () => {
-      return [
-        {
-          source: "/admin",
-          destination: "https://shark-app-4dsd9.ondigitalocean.app/admin",
-          permanent: true
-        }
-      ]
-    },
-    compiler: {
-      removeConsole: process.env.NODE_ENV === 'production' ? true : false,
-    },
-    webpack(config) {
-      // Exclude OptinMonster's script from automatic optimization
-      config.optimization.splitChunks = {
-        cacheGroups: {
-          optinMonsterGroup: {
-            test: /a\.omappapi\.com/,
-            name: 'optinMonster',
-            chunks: 'all',
-            enforce: true,
-          },
-        },
-      };
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+    redirects:async()=>{
+        return[
+            {
+                source:"/admin",
+                destination:"https://shark-app-4dsd9.ondigitalocean.app/admin",
+                permanent:true
+            }
+        ]
+    }
+};
+
+export default nextConfig;
